92a4f76096ad8f1cebf8ff0a0fdecbf24c95f67e
muraz-69 <andrewmurabu@gmail.com>
2023-11-27 16:01:21 +0300

    first project

---
+++ myapp/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+from myapp.forms import EmployeeForm
+from myapp.models import Employee
+
+# Create your views here.
+def home(request):
+    return render(request, 'index.html')
+
+
+def mygallery(request):
+    return render(request, 'gallery.html')
+
+
+def contactus(request):
+    return render(request, 'contact.html')
+
+
+def aboutus(request):
+    return render(request, 'about.html')
+
+
+def join(request):
+    if request.method == 'POST':
+        form = EmployeeForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, 'join.html')
+
+    else:
+        return render(request, 'join.html')
+
+def details(request):
+    members = Employee.objects.all()
+    return render(request, 'details.html', {'all':members})
